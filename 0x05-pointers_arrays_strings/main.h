a9bdc99e9b6edc020ea2a9b837597f23cc19eb33
Olaleye Ayomide <ayomideolaleye707@gmail.com>
2023-06-27 02:04:44 -0700

    reset_to_98 that takes a pointer to int

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
@@ -5,13 +7,14 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlem(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a,int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
+
